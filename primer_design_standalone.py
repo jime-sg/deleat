8e35a2895239c2a8bad7e7917d84c05f7b899a11
Jimena <jisogon@alumni.uv.es>
2019-10-22 13:28:36 +0200

    clases Primer y PrimerSet

--- primer_design_standalone.py
+++ primer_design_standalone.py
@@ -7,8 +7,8 @@
 import primer3
 from Bio import SeqIO
 from Bio.Restriction import BamHI
-from Bio.Seq import Seq
-from regions import Region
+from Bio.SeqRecord import SeqRecord
+from regions import Region, Primer, PrimerSet
 
 
 def check_BamHItargets_and_repeats(seq, seq_name, dir):
@@ -63,23 +63,29 @@ def design_primers(start, end):
     return results
 
 
-def parse_p3results(results_dict):
+def write_primer_pairs(primer_dict):
     results_str = "N\tsize\tF_start-F_end..R_start-R_end\tseq_F\tseq_R\tTM_F (ºC)\tTM_R (ºC)\n"
-    for n in range(0, results_dict["PRIMER_PAIR_NUM_RETURNED"]):
-        prod_size = results_dict["PRIMER_PAIR_%d_PRODUCT_SIZE" % n]
-        lp_start = results_dict["PRIMER_LEFT_%d" % n][0]
-        lp_end = results_dict["PRIMER_LEFT_%d" % n][0] + results_dict["PRIMER_LEFT_%d" % n][1] - 1
-        rp_start = results_dict["PRIMER_RIGHT_%d" % n][0]
-        rp_end = results_dict["PRIMER_RIGHT_%d" % n][0] + results_dict["PRIMER_RIGHT_%d" % n][1] - 1
-        lp_seq = results_dict["PRIMER_LEFT_%d_SEQUENCE" % n]
-        rp_seq = results_dict["PRIMER_RIGHT_%d_SEQUENCE" % n]
-        lp_tm = results_dict["PRIMER_LEFT_%d_TM" % n]
-        rp_tm = results_dict["PRIMER_RIGHT_%d_TM" % n]
+    n_pairs = len(primer_dict)//2
+    for n in range(n_pairs):
+        lp = primer_dict["LEFT_%d" % n]
+        rp = primer_dict["RIGHT_%d" % n]
+        prod_size = rp.e() - lp.s() + 1
 
         results_str += "%d\t%d\t%d-%d..%d-%d\t%-24s\t%-24s\t%.1f\t%.1f\n" \
-                       % (n + 1, prod_size, lp_start, lp_end, rp_start, rp_end, lp_seq, rp_seq, lp_tm, rp_tm)
+                       % (n+1, prod_size, lp.s(), lp.e(), rp.s(), rp.e(), lp.seq(), rp.seq(), lp.tm(), rp.tm())
 
     return results_str
+
+
+def save_pcr_regions(regions_dict, dir):
+    with open(dir + "PCR1.fna", "w") as f1, open(dir + "PCR2.fna", "w") as f2:
+        pcr1 = regions_dict["PCR1"]
+        pcr1 = SeqRecord(pcr1.subseq(), id="PCR1", description="%d:%d" % (pcr1.s(), pcr1.e()))
+        SeqIO.write(pcr1, f1, "fasta")
+        pcr2 = regions_dict["PCR2"]
+        pcr2 = SeqRecord(pcr2.subseq(), id="PCR2", description="%d:%d" % (pcr2.s(), pcr2.e()))
+        SeqIO.write(pcr2, f2, "fasta")
+
 
 
 GENOME = "/home/jimena/Bartonella/NC_005955.fna"
@@ -89,75 +95,83 @@ MARGIN_SIZE = 850
 
 sep = "-" * 80 + "\n"
 
-genome = SeqIO.read(GENOME, "fasta")
-log = open(LOG_DIR + "primer_design.txt", "w")
 
-# define initial regions
-## desired deletion
-del_region = Region(genome, DEL_COORDS)
-
-## deletion + margins (for PCR1/2)
-del_plusmargins_coords = (del_region.s() - MARGIN_SIZE,
-                          del_region.e() + MARGIN_SIZE)
-del_plusmargins = Region(genome, del_plusmargins_coords)
-
-## margin 1
-margin1_coords = (del_plusmargins.s(),
-                  del_region.s())
-margin1 = Region(genome, margin1_coords)
-
-## margin 2
-margin2_coords = (del_region.e(),
-                  del_plusmargins.e())
-margin2 = Region(genome, margin2_coords)
-
-# check margin region quality
-log.write(sep + "Checking margins...\n")
-log.write("Initial locations:\n\tMargin 1 (left):  %d - %d\n\tMargin 2 (right): %d - %d\n"
-          % (margin1.s(), margin1.e(), margin2.s(), margin2.e()))
-check_BamHItargets_and_repeats(margin1, "margin 1", dir="right")
-check_BamHItargets_and_repeats(margin2, "margin 2", dir="left")
-log.write("\nMargin quality check: done.")
-log.write("\nFinal locations:\n\tMargin 1 (left):  %d - %d\n\tMargin 2 (right): %d - %d\n"
-          % (margin1.s(), margin1.e(), margin2.s(), margin2.e()))
-
-# log.close()
-# exit()
+if __name__ == "__main__":
+    genome = SeqIO.read(GENOME, "fasta")
+    log = open(LOG_DIR + "primer_design.txt", "w")
 
 
-# primer design
-log.write(sep + "Designing primers...\n")
-results_margin1 = design_primers(margin1.s(), margin1.e())
-results_margin2 = design_primers(margin2.s(), margin2.e())
-log.write("\nBest primer pairs for margin 1 (left):\n")
-log.write(parse_p3results(results_margin1))
-log.write("\nBest primer pairs for margin 2 (right):\n")
-log.write(parse_p3results(results_margin2))
+    # define initial regions
+    del_region = Region(DEL_COORDS, genome)
+    del_plusmargins_coords = (del_region.s() - MARGIN_SIZE,
+                              del_region.e() + MARGIN_SIZE)
+    del_plusmargins = Region(del_plusmargins_coords, genome)
+    margin1_coords = (del_plusmargins.s(),
+                      del_region.s())
+    margin1 = Region(margin1_coords, genome)
+    margin2_coords = (del_region.e(),
+                      del_plusmargins.e())
+    margin2 = Region(margin2_coords, genome)
 
-primers_raw = {
-    "PCR1F": results_margin1["PRIMER_LEFT_0_SEQUENCE"].upper(),
-    "PCR1R": results_margin1["PRIMER_RIGHT_0_SEQUENCE"].upper(),
-    "PCR2F": results_margin2["PRIMER_LEFT_0_SEQUENCE"].upper(),
-    "PCR2R": results_margin2["PRIMER_RIGHT_0_SEQUENCE"].upper()
-}
 
-primers_tailed = {
-    "PCR1F": "gcacggatcc" + primers_raw["PCR1F"],
-    "PCR1R": primers_raw["PCR1R"],
-    "PCR2F": Seq(primers_raw["PCR1R"]).reverse_complement().lower() + primers_raw["PCR2F"],
-    "PCR2R": "gcacggatcc" + primers_raw["PCR2R"]
-}
+    # check margin region quality
+    log.write(sep + "Checking margins...\n")
+    log.write("Initial locations:\n\tMargin 1 (left):  %d - %d\n\tMargin 2 (right): %d - %d\n"
+              % (margin1.s(), margin1.e(), margin2.s(), margin2.e()))
+    check_BamHItargets_and_repeats(margin1, "margin 1", dir="right")
+    check_BamHItargets_and_repeats(margin2, "margin 2", dir="left")
+    log.write("\nMargin quality check: done.")
+    log.write("\nFinal locations:\n\tMargin 1 (left):  %d - %d\n\tMargin 2 (right): %d - %d\n"
+              % (margin1.s(), margin1.e(), margin2.s(), margin2.e()))
 
-log.write("\nSelected first pairs of primers:\n")
-for a, b in primers_raw.items():
-    log.write("%s: %s\n" % (a, b))
 
-log.write("\nAdded tails:\n")
-for a, b in primers_tailed.items():
-    log.write("%s: %s\n" % (a, b))
+    # primer design
+    log.write(sep + "Designing primers...\n")
+    all_primers = {1: {}, 2: {}}
+    for i, margin in enumerate((margin1, margin2)):
+        p3_results = design_primers(margin.s(), margin.e())
+        n_pairs = p3_results["PRIMER_PAIR_NUM_RETURNED"]
+        for j in range(n_pairs):
+            for k in ("LEFT", "RIGHT"):
+                primer = Primer(p3_results, j, k)
+                all_primers[i+1][primer.id] = primer
 
-# PCR1/2 region design
-# check for Bam targets
+    log.write("\nBest primer pairs for margin 1 (left):\n")
+    log.write(write_primer_pairs(all_primers[1]))
+    log.write("\nBest primer pairs for margin 2 (right):\n")
+    log.write(write_primer_pairs(all_primers[2]))
 
-log.write(sep)
-log.close()
+    chosen_ids = ["LEFT_0", "RIGHT_0", "LEFT_0", "RIGHT_0"]
+    chosen_primers = {
+        "1F": all_primers[1][chosen_ids[0]],
+        "1R": all_primers[1][chosen_ids[1]],
+        "2F": all_primers[2][chosen_ids[2]],
+        "2R": all_primers[2][chosen_ids[3]]
+    }
+
+    megapriming = PrimerSet(chosen_primers)
+    log.write("\nSelected pairs of primers:\n")
+    for name, primer in megapriming.primers_raw_dict.items():
+        log.write("%s: %s\n" % (name, primer.seq()))
+
+    megapriming.add_tails()
+    log.write("\nAdded tails:\n")
+    for name, primer in megapriming.primers_tailed_dict.items():
+        log.write("%s: %s\n" % (name, primer))
+
+    pcr_regions = megapriming.get_PCR_regions(genome)
+    log.write("\nDetermined PCR regions for megapriming:\n")
+    log.write("\tPCR1 (left):  %d - %d\n\tPCR2 (right): %d - %d\n"
+              % (pcr_regions["PCR1"].s(), pcr_regions["PCR1"].e(),
+                 pcr_regions["PCR2"].s(), pcr_regions["PCR2"].e()))
+
+    save_pcr_regions(pcr_regions, LOG_DIR)
+    log.write("\nPCR region sequences saved at %s.\n" % LOG_DIR)
+
+
+    # PCR1/2 region design
+    # check for Bam targets
+
+
+    log.write(sep)
+    log.close()
--- regions.py
+++ regions.py
@@ -5,22 +5,23 @@
 """
 
 from Bio.SeqFeature import SeqFeature, FeatureLocation
+from Bio.Seq import Seq
 
 
 class Region:
     """
     Manage subsequence basic properties.
     """
-    def __init__(self, seq, coords):
+    def __init__(self, coords, global_seq):
         """
 
         :rtype: Bio.SeqRecord
         """
-        self.origseq = seq
         self.start = coords[0]
         self.end = coords[1]
+        self.globalseq = global_seq
         self.seqfeature = SeqFeature(FeatureLocation(self.start, self.end))
-        self.subsequence = self.seqfeature.extract(seq).seq
+        self.subsequence = self.seqfeature.extract(global_seq).seq
 
     def subseq(self):
         return self.subsequence
@@ -35,5 +36,80 @@ class Region:
         elif dir == "left":
             displ = -(len(self.subsequence) - pos)
 
-        self.__init__(self.origseq,
-                      (self.start + displ, self.end + displ))
+        self.__init__((self.start + displ, self.end + displ),
+                      self.globalseq)
+
+
+class Primer:
+    def __init__(self, p3results_dict, primer_n, primer_dir):
+        self.id = primer_dir + "_" + str(primer_n)
+        self.start = p3results_dict["PRIMER_%s" % self.id][0]
+        if primer_dir == "LEFT":
+            self.end = self.start + p3results_dict["PRIMER_%s" % self.id][1] - 1
+        elif primer_dir == "RIGHT":
+            self.end = self.start - p3results_dict["PRIMER_%s" % self.id][1] + 1
+        self.sequence = Seq(p3results_dict["PRIMER_%s_SEQUENCE" % self.id].upper())
+        self.temp = p3results_dict["PRIMER_%s_TM" % self.id]
+        self.penalty = p3results_dict["PRIMER_%s_PENALTY" % self.id]
+        self.gc = p3results_dict["PRIMER_%s_GC_PERCENT" % self.id]
+        self.self_any_th = p3results_dict["PRIMER_%s_SELF_ANY_TH" % self.id]
+        self.self_end_th = p3results_dict["PRIMER_%s_SELF_END_TH" % self.id]
+        self.hairpin_th = p3results_dict["PRIMER_%s_HAIRPIN_TH" % self.id]
+        self.end_stability = p3results_dict["PRIMER_%s_END_STABILITY" % self.id]
+
+    def seq(self):
+        return self.sequence
+
+    def s(self):
+        return self.start
+
+    def e(self):
+        return self.end
+
+    def tm(self):
+        return self.temp
+
+
+class PrimerSet:
+    def __init__(self, primer_dict):
+        self.PCR1F = primer_dict["1F"]
+        self.PCR1R = primer_dict["1R"]
+        self.PCR2F = primer_dict["2F"]
+        self.PCR2R = primer_dict["2R"]
+
+        self.primers_raw_dict = {
+            "PCR1_F": self.PCR1F,
+            "PCR1_R": self.PCR1R,
+            "PCR2_F": self.PCR2R,
+            "PCR2_R": self.PCR2R
+        }
+
+    def add_tails(self):
+        # PCR1_Bam-F: add BamHI target site at 5'
+        self.PCR1Ft = Seq("gcacggatcc") + self.PCR1F.seq()
+        # PCR1_R: unchanged
+        self.PCR1Rt = self.PCR1R.seq()
+        # PCR2_F: add revcomp of PCR1_R at 5'
+        self.PCR2Ft = self.PCR2F.seq().reverse_complement().lower() + self.PCR2F.seq()
+        # PCR2_Bam-R: add BamHI target site at 5'
+        self.PCR2Rt = Seq("gcacggatcc") + self.PCR2R.seq()
+
+        self.primers_tailed_dict = {
+            "PCR1_Ft": self.PCR1Ft,
+            "PCR1_Rt": self.PCR1Rt,
+            "PCR2_Ft": self.PCR2Ft,
+            "PCR2_Rt": self.PCR2Rt
+        }
+
+    def get_PCR_regions(self, global_seq):
+        pcr1_start = self.PCR1F.s()
+        pcr1_end = self.PCR1R.e()
+        pcr2_start = self.PCR2F.s()
+        pcr2_end = self.PCR2R.e()
+        self.PCR1_region = Region((pcr1_start, pcr1_end), global_seq)
+        self.PCR2_region = Region((pcr2_start, pcr2_end), global_seq)
+        pcr_dict = {"PCR1": self.PCR1_region,
+                    "PCR2": self.PCR2_region}
+
+        return pcr_dict
+
