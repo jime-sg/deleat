b1e1e056ea737288c6cad6d6ef823f15c6ce2996
jime-sg <jime.sg6@gmail.com>
2020-03-27 13:54:16 +0100

    nonessential_genes.py: integrar resultados con función dummy (+) y guardar .gbm

--- nonessential_genes.py
+++ nonessential_genes.py
@@ -5,6 +5,7 @@
 """
 
 from argparse import ArgumentParser
+from sys import argv  # FIXME
 
 from Bio import SeqIO
 from Bio.Seq import Seq
@@ -46,22 +47,57 @@ def make_description(feature):
     return description
 
 
+def integrate_scores(seqfeature_s, geptop_s):
+    a = int(seqfeature_s.split(";")[0].split(",")[1])
+    b = int(seqfeature_s.split(";")[1].split(",")[1])
+    c = float(seqfeature_s.split(";")[2].split(",")[1])
+    d = geptop_s
+    score = a + b + c + d
+    return score
+
+
 if __name__ == "__main__":
     # Parse command-line arguments
     parser = ArgumentParser(
         prog="nonessential-genes",
         description=""  # FIXME
     )
-    GENBANK = "/home/jimena/Bartonella/NC_005955.gb"
-    PROTEOME = "/home/jimena/Escritorio/proteoma.faa"
+    # GENBANK = "/home/jimena/Bartonella/NC_005955.gb"
+    # PROTEOME = "/home/jimena/Escritorio/proteoma.faa"
+    GENBANK = argv[1]
+    PROTEOME = argv[2]
+    OUT_GENBANK = argv[3]
 
     save_proteome(
         gb_file=GENBANK,
         out_file=PROTEOME
     )
 
-    # seqfeatures_results = seqfeatures.run(GENBANK)
-    # geptop_results = geptop.run(PROTEOME, )  # FIXME
+    # Add seqfeatures results to annotation file
+    mod_annotation = seqfeatures.run(GENBANK)
 
+    # Add Geptop results to annotation file
+    geptop_results = geptop.run(
+        query_file=PROTEOME,
+        deg_path="/home/jimena/Bartonella/DEGdb/deg_byorg/temp",
+        cv_path="/home/jimena/Bartonella/DEGdb/cv",
+        cutoff=0.24,
+        n_proc=4,
+        out_path="/home/jimena/Escritorio"
+    )  # FIXME (leer argumentos de argparser)
+    for gene in mod_annotation.features:
+        if gene.type == "CDS" and "translation" in gene.qualifiers:
+            locus_tag = gene.qualifiers["locus_tag"][0]
+            geptop_score = geptop_results[locus_tag][0]
+            gene.qualifiers["geptop"] = [geptop_score]
 
+            # Add integrated score to annotation file
+            seqfeature_scores = gene.qualifiers["seqfeatures"][0]
+            gene.qualifiers["essentiality"] = [integrate_scores(
+                seqfeature_scores,
+                geptop_score
+            )]
+        # TODO elif -> otros tipos de genes
 
+    # Write modified annotation file
+    SeqIO.write(mod_annotation, OUT_GENBANK, "genbank")
