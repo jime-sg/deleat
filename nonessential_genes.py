05e35d2f40489293ced5b65fb40a3b37260d8b2a
jime-sg <jime.sg6@gmail.com>
2020-05-26 13:35:40 +0200

    nonessential_genes.py: guardar proteoma aa+nt, calcular hebra, resultados en pd.DF

--- nonessential_genes.py
+++ nonessential_genes.py
@@ -10,31 +10,34 @@ import os
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+import pandas as pd
 
 import seqfeatures
 import geptop
 
 
-def save_proteome(gb_file, out_file):
-    proteins = []
+def extract_cds(gb_file, out_aa, out_nt):
+    proteins_aa = []
+    proteins_nt = []
     annot = SeqIO.read(gb_file, "genbank")
     for feature in annot.features:
-        if feature.type == "CDS" and "translation" in feature.qualifiers:
-            protein = SeqRecord(
+        if (feature.type == "CDS" and
+                "translation" in feature.qualifiers and
+                "locus_tag" in feature.qualifiers):
+            protein_aa = SeqRecord(
                 seq=Seq(feature.qualifiers["translation"][0]),
-                id=make_id(feature),
+                id=feature.qualifiers["locus_tag"][0],
                 description=make_description(feature)
             )
-            proteins.append(protein)
-    SeqIO.write(proteins, out_file, "fasta")
-
-
-def make_id(feature):
-    if "locus_tag" in feature.qualifiers:
-        id_ = feature.qualifiers["locus_tag"][0]
-    else:
-        id_ = "(no locus_tag)"
-    return id_
+            protein_nt = SeqRecord(
+                seq=feature.extract(annot.seq),  # FIXME
+                id=feature.qualifiers["locus_tag"][0],
+                description=make_description(feature)
+            )
+            proteins_aa.append(protein_aa)
+            proteins_nt.append(protein_nt)
+    SeqIO.write(proteins_aa, out_aa, "fasta")
+    SeqIO.write(proteins_nt, out_nt, "fasta")
 
 
 def make_description(feature):
@@ -45,6 +48,29 @@ def make_description(feature):
     location = "".join(str(part) for part in feature.location.parts)
     description = "%s %s" % (product, location)
     return description
+
+
+def strand(proteome, ori, ter):  # FIXME: comprobar
+    proteins = list(SeqIO.read(proteome, "fasta"))
+    strand_results = pd.DataFrame(
+        index=[prot.id for prot in proteins],
+        columns=["strand_lead"]
+    )
+    for prot in proteins:
+        location = prot.description.split()[-1]
+        start = int(location.split(":")[0][1:])
+        sign = location[-2]
+        if start > ori or start < ter:  # 0-180º replichore
+            if sign == "+":  # leading strand
+                strand_results.loc[prot.id, "strand_lead"] = 1
+            else:  # lagging strand
+                strand_results.loc[prot.id, "strand_lead"] = 0
+        else:  # 180-360º replichore
+            if sign == "+":  # lagging strand
+                strand_results.loc[prot.id, "strand_lead"] = 0
+            else:  # leading strand
+                strand_results.loc[prot.id, "strand_lead"] = 1
+    return strand_results
 
 
 def integrate_scores(seqfeature_s, geptop_s):  # FIXME
@@ -85,7 +111,6 @@ if __name__ == "__main__":
     OUT_DIR = args.OUT_DIR
     genbank_id = os.path.splitext(os.path.basename(GENBANK))[0]
     GENBANK_M1 = os.path.join(OUT_DIR, genbank_id + ".gbm1")
-    PROTEOME = os.path.join(OUT_DIR, "proteome.faa")
     DEG = args.DEG
     CV = args.CV
     GEPTOP_CUTOFF = args.GEPTOP_CUTOFF
@@ -94,20 +119,35 @@ if __name__ == "__main__":
     # Check input
     # TODO
 
-    save_proteome(gb_file=GENBANK, out_file=PROTEOME)
+    # Get ori + ter coordinates
+    # TODO
+    ori = 0
+    ter = 500000
 
-    # Add seqfeatures results to annotation file
-    genbank_m1 = seqfeatures.run(GENBANK)
+    # Extract proteome
+    proteome_aa = os.path.join(OUT_DIR, "proteome.faa")
+    proteome_nt = os.path.join(OUT_DIR, "proteome.fna")
+    extract_cds(gb_file=GENBANK, out_aa=proteome_aa, out_nt=proteome_nt)
 
-    # Add Geptop results to annotation file
+    # Get strand data
+    results = strand(proteome_aa, ori=ori, ter=ter)
+
+    # Get Geptop scores
     geptop_results = geptop.run(
-        query_file=PROTEOME,
+        query_file=proteome_aa,
         deg_path=DEG,
         cv_path=CV,
         cutoff=GEPTOP_CUTOFF,
         n_proc=NPROC,
         out_path=OUT_DIR
     )
+    for gene, score in geptop_results.items():
+        results.loc[gene, "geptop"] = score[0]
+
+
+    # Add seqfeatures results to annotation file
+    genbank_m1 = seqfeatures.run(GENBANK)
+
     for gene in genbank_m1.features:
         if gene.type == "CDS" and "translation" in gene.qualifiers:
             locus_tag = gene.qualifiers["locus_tag"][0]
