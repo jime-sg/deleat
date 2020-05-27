5868db28b4ffbfc015ed791c8bba857c81d1f718
jime-sg <jime.sg6@gmail.com>
2020-05-27 13:15:19 +0200

    codonw.py: correcciones

--- codonw.py
+++ codonw.py
@@ -11,21 +11,28 @@ import pandas as pd
 
 
 def run(fasta, feature_list, remove_blk=True):
-    prefix = os.path.splitext(fasta)[0]
-    out = prefix + ".out"
-    blk = prefix + ".blk"
-    subprocess.call(
-        ["codonw", fasta,
-         "-nomenu", "-nowarn", "-silent", "-machine", "-t,",
-         *feature_list]
-    )
-    results = read_table(out, feature_list)
+    print("Running CodonW...")
+    out_dir = os.path.split(fasta)[0]
+    out = os.path.join(out_dir, "codonw.out")
+    blk = os.path.join(out_dir, "codonw.blk")
+    log = os.path.join(out_dir, "codonw_log.txt")
+    with open(log, "w") as fo:
+        subprocess.run(
+            ["codonw", fasta, out, blk,
+             "-nomenu", "-nowarn", "-silent", "-machine", "-t,",
+             *feature_list],
+            stdout=fo, stderr=fo
+        )
+    results = read_table(out)
     if remove_blk:
         os.remove(blk)
+    print("Done.")
     return results
 
 
-def read_table(csv, columns):
-    table = pd.read_csv(csv, usecols=["title", *columns], index_col="title")
+def read_table(csv):
+    table = pd.read_csv(csv, index_col="title")
+    table = table.iloc[:, :-1]  # Remove empty last column
     table.Nc = pd.to_numeric(table.Nc, errors="coerce")  # Missing values > NaN
+    table.index = table.index.map(lambda x: x.split()[0])  # Index = locus_tag
     return table
